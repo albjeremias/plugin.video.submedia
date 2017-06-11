2bebb5bc3b80499bdff621f5f7dcf90b58f3b485
Albatroz Jeremias <ajeremias@disroot.org>
2017-06-11 22:47:49 +0200

    testing

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
+# 
+# from resources.lib import kodilogging
+# from resources.lib import plugin
+# 
+# import logging
+# import xbmcaddon
+# 
+# # Keep this file to a minimum, as Kodi
+# # doesn't keep a compiled copy of this
+# ADDON = xbmcaddon.Addon()
+# kodilogging.config()
+# 
+# plugin.run()
 
-from resources.lib import kodilogging
-from resources.lib import plugin
+import sys
+import xbmcgui
+import xbmcplugin
+import CommonFunctions
+import urllib2
 
-import logging
-import xbmcaddon
+addon_handle = int(sys.argv[1])
 
-# Keep this file to a minimum, as Kodi
-# doesn't keep a compiled copy of this
-ADDON = xbmcaddon.Addon()
-kodilogging.config()
+xbmcplugin.setContent(addon_handle, 'movies')
 
-plugin.run()
+# common = CommonFunctions
+# common.plugin = "Submedia Kodi v1.0"
+# 
+urllib2.urlopen('http://sub.media')
 
+url = 'https://archive.org/download/FlagBurn/HowToBurnAnAmericanFlag720p.mp4'
+li = xbmcgui.ListItem('My Second Video!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
+xbmcplugin.endOfDirectory(addon_handle)
