de72aec4f7c6f7086952c41b654536c284efd8c0
Albatroz Jeremias <ajeremias@disroot.org>
2017-06-13 00:09:31 +0200

    fixing and routing

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
-# -*- coding: utf-8 -*-
-# 
-# from resources.lib import kodilogging
-# from resources.lib import plugin
-# 
-# import logging
-# import xbmcaddon
-# 
-# # Keep this file to a minimum, as Kodi
-# # doesn't keep a compiled copy of this
-# ADDON = xbmcaddon.Addon()
-# kodilogging.config()
-# 
-# plugin.run()
-
 import sys
 import re
+import routing
+import xbmcaddon
 import xbmcgui
 import xbmcplugin
+from xbmcgui import ListItem
+from xbmcplugin import addDirectoryItem, endOfDirectory
 import CommonFunctions as xbmc_common
+# from resources.lib import kodiutils
+# from resources.lib import kodilogging
+# from xbmcgui import ListItem
+# from xbmcplugin import addDirectoryItem, endOfDirectory
+# 
+# # 
 
+
+# # 
+# # import routing
+
+# 
 def getVideoUrl(articleUrl):
-    print articleUrl
     articleHTML = xbmc_common.fetchPage({"link": articleUrl})
     regex = r"href=\"(.+?mp4)\""
     content = articleHTML['content']
     matches = re.finditer(regex, content)
-    # content[match.start(groupNum):match.end(groupNum)]
     for matchNum, match in enumerate(matches):
         matchNum = matchNum + 1
         for groupNum in range(0, len(match.groups())):
             groupNum = groupNum + 1
-            print content[match.start(groupNum):match.end(groupNum)]
             return content[match.start(groupNum):match.end(groupNum)]
     return ''
     
 def getCategory(category):
     categoryHTML = xbmc_common.fetchPage({"link": "https://sub.media/c/" + category + "/"})
-    # print repr(result['content'])
     category = xbmc_common.parseDOM(categoryHTML['content'], 'main', attrs = {'id': 'main'})
     articles = xbmc_common.parseDOM(category, 'article')
     
@@ -45,26 +41,37 @@ def getCategory(category):
         title = xbmc_common.parseDOM(article, 'a', ret = 'title')
         articleUrl = xbmc_common.parseDOM(article, 'a', ret = 'ref')
         desc = xbmc_common.parseDOM(article, 'p')
-        print repr(title)
-        print repr(desc)
-        print repr(img)
         
         urlVideo = getVideoUrl(articleUrl[0])
-        print "video ::: "
-        print urlVideo
-        # url = 'https://archive.org/download/FlagBurn/HowToBurnAnAmericanFlag720p.mp4'
-        li = xbmcgui.ListItem(title[0].encode('utf-8'), iconImage='DefaultVideo.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=urlVideo, listitem=li)
-        # print repr(ret[0])
+        
+        # print title[0].encode('utf-8')
+        # print urlVideo
+        if len(urlVideo) > 0:
+            li = xbmcgui.ListItem(title[0].encode('utf-8'), iconImage=img[0])
+            addDirectoryItem(handle=plugin.handle, url=urlVideo, listitem=li)
+        else:
+            #http://kodi.wiki/view/Add-on:YouTube
+            print title[0].encode('utf-8')
+            print urlVideo
+            print "failed"
 
-addon_handle = int(sys.argv[1])
+plugin = routing.Plugin()
 
-xbmcplugin.setContent(addon_handle, 'movies')
+@plugin.route('/')
+def index():
+    addDirectoryItem(plugin.handle, plugin.url_for(show_category, "trouble"), ListItem("Trouble"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(show_category, "stimulator"), ListItem("Stimulator"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(show_category, "a-is-for-anarchy"), ListItem("A is for Anarchy"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(show_category, "video-ninjas"), ListItem("Video Ninjas"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(show_category, "submedia-films"), ListItem("Submedia Films"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(show_category, "featured-content"), ListItem("Featured Content"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(show_category, "collaborations"), ListItem("Collaborations"), True)
+    endOfDirectory(plugin.handle)
 
-getCategory("trouble")
+@plugin.route('/category/<category_id>')
+def show_category(category_id):
+    getCategory(category_id)
+    endOfDirectory(plugin.handle)
 
-
-
-xbmcplugin.endOfDirectory(addon_handle)
-
-
+if __name__ == '__main__':
+    plugin.run()
